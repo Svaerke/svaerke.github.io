b45b841d01662a78d76de5b1d70f9c523093d3a7
Morten Sværke Andersen <morten.svaerke@gmail.com>
2020-08-22 14:03:27 +0200

    Fixed an issue with files

---
+++ build/precache-manifest.5e0e91cc7515829587b456b9ba2e280d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7e521b52312f10d27e5fb3fa7dfd54e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbd5d5134a838db4d90d",
+    "url": "/static/css/main.88208d1e.chunk.css"
+  },
+  {
+    "revision": "3bdf5a1d5db2bfbdc14f",
+    "url": "/static/js/2.b7d47fe7.chunk.js"
+  },
+  {
+    "revision": "fbd5d5134a838db4d90d",
+    "url": "/static/js/main.07f6038d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
